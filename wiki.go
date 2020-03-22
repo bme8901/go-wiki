c761efdc52f239d5fce9df05a9a5bc711bbe58ab
Ben ElDarragi <eldarragi@Bens-MacBook-Pro.local>
2020-03-21 19:22:45 -0500

    http handling.

--- wiki.go
+++ wiki.go
@@ -3,20 +3,40 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 type Page struct {
 	Title string
 	Body []byte
+}
 
-	func (p *Page) save() error {
-		filename := p.Title + ".txt"
-		return ioutil.WriteFile(filename, p.body, 0600)
-	}
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
 
-	func loadPage(title string) *Page {
-		filename := title + ".txt"
-		body, _ := ioutil.ReadFile(filename)
-		return &Page{Title: title, Body: body}
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
 	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func main() {
+	p1 := &Page{Title: "test", Body: []byte("This is a test page.")}
+	p1.save()
+	p2, _ := loadPage("test")
+	fmt.Println(string(p2.Body))
+	http.HandleFunc("/view/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
